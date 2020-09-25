2f79ac851199813ef7d05ea4d004eceb04264bf0
bullet-designs <71181598+bullet-designs@users.noreply.github.com>
2020-09-25 02:32:54 +0200

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -9,6 +9,20 @@
 
     @yield('postContent')
 
+<div class="row" id="imjo">
+  <div class="cell" id="iftt">
+    <img id="ihvb" src="//placehold.it/350x250/459ba8/fff/image2.jpg"/>
+  </div>
+  <div class="cell" id="isyr">
+    <video allowfullscreen="allowfullscreen" id="i4c4" src="img/video2.webm" controls="controls">
+    </video>
+  </div>
+  <div class="cell" id="id5q">
+    <img id="ivb1" src="//placehold.it/350x250/459ba8/fff/image2.jpg"/>
+  </div>
+</div>
+
+
     <hr>
 
     @include('_partials.share')

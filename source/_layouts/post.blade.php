4f2c4198eb89f9ea83bd5e82a3b8cf3f7f5dda1d
bullet-designs <71181598+bullet-designs@users.noreply.github.com>
2020-09-25 02:01:51 +0200

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,17 +5,6 @@
 @section('content')
     <h1>{{ $page->title }}</h1>
 
-    @if ($page->image)
-        <img src="{{ $page->image }}" style="object-fit: cover; height: 250px; width: 100%;">
-    @endif
-
-    <p>
-        <strong>{{ $page->prettyDate('F j, Y') }}</strong><br>
-        @foreach ($page->tags as $tag)
-            <a href="/tags/{{ $tag }}">{{ $tag }}</a>
-            {{ $loop->last ? '' : '-' }}
-        @endforeach
-    </p>
 
 
     @yield('postContent')
